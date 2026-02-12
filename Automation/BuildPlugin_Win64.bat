aac50550f5f3e8bc5073c6827dd7651f4c00935c
InsaneDoggo <doter.ua@gmail.com>
2026-02-12 22:22:41 +0200

    chore: fix plugin package destination dir name

--- Automation/BuildPlugin_Win64.bat
+++ Automation/BuildPlugin_Win64.bat
@@ -5,7 +5,7 @@ SET RunUAT_PATH=f:\ue_bin\UE_5.3\Engine\Build\BatchFiles\RunUAT.bat
 
 %RunUAT_PATH% BuildPlugin ^
  -Plugin="%~dp0\..\TwoHeadUtilities.uplugin" ^
- -Package="%~dp0\..\Build\TwoHeadUtilities_Package" ^
+ -Package="%~dp0\..\Build\TwoHeadUtilities" ^
  -TargetPlatforms=Win64
 
 echo Done!
